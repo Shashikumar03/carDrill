4d99123db592bcb80067e69527b96960dcb3c4aa
Shashi kushwaha <shashikumarkushwaha3@gmail.com>
2023-11-22 10:51:53 +0530

    modify

--- Test/testProblem1.js
+++ Test/testProblem1.js
@@ -1,5 +1,6 @@
-const { inventory } = require("../inventory");
+const inventory = require("../inventory");
 const { problem1 } = require("../problem1");
-const a = problem1(inventory);
+const ID = 33;
+const a = problem1(inventory, ID);
 
-console.log(`car with id: ${a.id} ${a.car_year} ${a.car_make} ${a.car_model}`);
+// console.log(`car with id: ${a.id} ${a.car_year} ${a.car_make} ${a.car_model}`);
--- Test/testProblem5.js
+++ Test/testProblem5.js
@@ -1,6 +1,7 @@
-const { problem5 } = require(".././problem5");
-const { inventory } = require(".././inventory");
+const { problem5 } = require("../problem5");
+const { inventory } = require("../inventory");
+const TWO_THOUSAND = 2000;
 
-const olderCar = problem5(inventory);
+const olderCar = problem5(inventory, TWO_THOUSAND);
 console.log("total number of older car is " + " " + olderCar.length);
 console.log(olderCar);
